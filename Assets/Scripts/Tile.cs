f41faf854aa6b1afbbc10af6baa1b39e46b65382
Austin <133702741+Austincain1006@users.noreply.github.com>
2025-07-25 17:23:29 -0500

    Added Grid Highlights

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,19 +4,39 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color baseColor, accentColor;
     [SerializeField] private SpriteRenderer myRenderer;
+    [SerializeField] private GameObject highlight;
+    
 
-    private bool tempDebug = false;
+    private bool updateColor = false;
+
+
 
     public void initialize(bool isOffset)
     {
-        Debug.Log(isOffset);
-        tempDebug = isOffset;
+        updateColor = isOffset;
         myRenderer.color = (isOffset) ? accentColor : baseColor;
     }
 
 
     void Update()
     {
-        myRenderer.color = (tempDebug) ? accentColor : baseColor;
+        myRenderer.color = (updateColor) ? accentColor : baseColor;
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("Enter!");
+        highlight.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Exit!");
+        highlight.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log($"clicked on {name}!");
     }
 }
